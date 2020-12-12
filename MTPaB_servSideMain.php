a5ca57c829c19a45fb07d80b5f91559d5084c502
ullanor <pawel.nowacki95@gmail.com>
2020-12-12 21:39:53 +0100

    more improvements

--- MTPaB_servSideMain.php
+++ MTPaB_servSideMain.php
@@ -6,6 +6,11 @@ $f = fopen("MikroTestStop.txt", "w");
 fwrite($f,"5");
 fclose($f);
 }
+else if($type == 5){
+$f = fopen("MikroTestFinished.txt", "w");
+fwrite($f,"safe mode operation");
+fclose($f);
+}
 else{
 $f = fopen("MikroTestStart.txt", "w");
 fwrite($f,"2000");
